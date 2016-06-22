84052780fc7bd423fe51ed9d7f70f0cd62291436
liuwei <liuwei@party-time.cn>
2016-06-22 10:55:59 +0800

    add git ignore

---
+++ danmu_bms_api/src/main/java/cn/partytime/Application.java
@@ -0,0 +1,17 @@
+package cn.partytime;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by liuwei on 16/6/22.
+ */
+
+
+@ComponentScan("cn.partytime")
+@SpringBootApplication
+@Slf4j
+public class Application {
+
+}
