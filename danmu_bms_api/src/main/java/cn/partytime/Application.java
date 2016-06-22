d51e1de6e183409c2e224ecb1b8f2475764572bc
jushidai <liuwei@party-time.cn>
2016-06-22 11:07:39 +0800

    Dev (#1)
    * add git ignore
    
    * add spring boot application

---
+++ danmu_bms_api/src/main/java/cn/partytime/Application.java
@@ -0,0 +1,29 @@
+package cn.partytime;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by liuwei on 16/6/22.
+ */
+
+
+@ComponentScan("cn.partytime")
+@SpringBootApplication
+@Slf4j
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
