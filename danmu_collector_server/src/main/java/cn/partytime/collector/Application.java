7f1b8db3218265db9b74ffb945ae27db24eacfd0
soaryang <759620299@qq.com>
2016-06-22 18:57:52 +0800

    弹幕采集器代码提交 系统启动时候，包扫描修改

--- danmu_collector_server/src/main/java/cn/partytime/collector/Application.java
+++ danmu_collector_server/src/main/java/cn/partytime/collector/Application.java
@@ -4,7 +4,9 @@ package cn.partytime.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("cn.partytime")
 @SpringBootApplication
 public class Application {
 
